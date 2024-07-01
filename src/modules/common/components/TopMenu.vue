61d610168670db5dd0b8576964f9d392e9f6373b
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-07-01 17:25:04 -0500

    Estructura de los componentes

---
+++ src/modules/common/components/TopMenu.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex items-center justify-between p-4 bg-base-200">
+    <h2 class="text-lg font-bold text-blue-600">Notion</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import TopMenu from "@/modules/common/components/TopMenu.vue";
+import SideMenu from "@/modules/projects/components/SideMenu.vue";
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <!--    Top Menu  -->
+
+    <top-menu></top-menu>
+
+    <main>
+      <!--   side Menu   -->
+
+      <side-menu></side-menu>
+
+      <!--   router view   -->
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
