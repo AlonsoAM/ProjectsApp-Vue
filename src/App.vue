d91cc1f964c197c02e9460f067259f818ed762a3
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-07-01 17:06:37 -0500

    Iniciando el proyecto y configurando Tailwind y la estructura de carpetas

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
