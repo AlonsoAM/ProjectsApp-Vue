58a5f05594e2147e9923830ffe5bc68003197a7e
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-08-16 16:45:36 -0500

    redireccionar el proyecto y crear tabla

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 
-import {useRouter} from "vue-router";
 import BreadCrumbs from "@/modules/common/components/BreadCrumbs.vue";
 import {useProjectStore} from "@/modules/projects/store/projects.store";
 import {ref, watch} from "vue";
 import type {Project} from "@/modules/projects/interfaces/project.interface";
-
-const route = useRouter()
+import {useRouter} from "vue-router";
 
 interface Props {
   id: string
 }
+
+const router = useRouter()
 
 const props = defineProps<Props>()
 
@@ -19,10 +19,50 @@ const project = ref<Project | null>()
 
 watch(() => props.id, (id) => {
   project.value = projecStore.projectList.find(project => project.id === id)
+  if (project.value === undefined) {
+    router.push('/projects')
+  }
 }, {immediate: true})
 
 </script>
 
 <template>
-  <BreadCrumbs :name="project?.name ?? 'No name'"/>
+  <div class="w-full">
+    <section class="m-2">
+      <BreadCrumbs :name="project?.name ?? 'No name'"/>
+    </section>
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+          <tr class>
+            <th class="w-14">Completada</th>
+            <th>Tarea</th>
+            <th>Completada en</th>
+          </tr>
+          </thead>
+          <tbody>
+          <!-- row 1 -->
+          <tr class="hover">
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+          </tr>
+          <!-- row 2 -->
+          <tr class="hover">
+            <th></th>
+            <td>
+              <input type="text"
+                     class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                     placeholder="Nueva Tarea"/>
+            </td>
+            <td></td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
+
 </template>
